d6f1c0b2b0e95707f1fc5572185cfd9911c0c6e8
Lili Sidharta <sidharta.liliek@gmail.com>
2023-10-25 01:49:26 +0000

    initial commit

---
+++ reviews/views.py
@@ -0,0 +1,10 @@
+from django.views.generic import ListView
+from .models import Review
+
+
+class HomePageView(ListView):
+    template_name = "home.html"
+    model = Review
+
+
+# Create your views here.
